30565beb1ce0dfed84b1239e188e8a136b86b995
Taras <77995009+TaraShevchenko@users.noreply.github.com>
2023-07-17 22:30:37 +0300

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const helloWordElement = screen.getByText(/hello world/i);
+    const buttonElement = screen.getByRole('button');
+    const inputElement = screen.getByPlaceholderText(/input value/i);
+
+    expect(helloWordElement).toBeInTheDocument();
+    expect(buttonElement).toBeInTheDocument();
+    expect(inputElement).toBeInTheDocument();
+    expect(inputElement).toMatchSnapshot();
+    // screen.debug()
+  });
+
+})
